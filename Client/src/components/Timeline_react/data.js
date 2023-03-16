5775f65ec790efd4b7e666c3b93ba1e1ad293579
prabhat_021 <prabhatsahrawat010203@gmail.com>
2023-03-16 15:46:54 +0530

    timeline section

---
+++ Client/src/components/Timeline_react/data.js
@@ -0,0 +1,51 @@
+
+const timelineData = [
+    {
+        text: 'Inauguration',
+        date: '9:30am - 10:30am',
+        category: {
+			tag: 'Event 1',
+			color: '#FFDB14'
+		},
+        
+    },
+    {
+        text: 'Tech talks',
+        date: '10:30am - 11:30am',
+        category: {
+			tag: 'Event 2',
+			color: '#e17b77'
+		},
+        
+    },
+    {
+        text: 'Games',
+        date: '11:30am - 12:30pm',
+        category: {
+			tag: 'Event 3',
+			color: '#1DA1F2'
+		},
+       
+    },
+    {
+        text:
+            'Speaker session',
+        date: '1:30pm - 2:30pm',
+        category: {
+			tag: 'Event 4',
+			color: '#018f69'
+		},
+        
+    },
+    {
+        text: 'Reward Ceremony',
+        date: '2:30pm - 3:30pm',
+        category: {
+			tag: 'Event 5',
+			color: '#018f69'
+		},
+        
+    }
+]
+
+export default timelineData;
